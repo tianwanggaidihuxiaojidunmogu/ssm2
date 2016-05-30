694d44a7c9c1088e6b560a7f03780ef6f98c2f1b
laupaul <l_m_peng@163.com>
2016-05-30 16:42:01 +0800

    commit

--- src/main/core/com/racing/mybatis/commons/GenerateConfigUtils.java
+++
@@ -1,5 +0,0 @@
-package com.racing.mybatis.commons;
-
-public class GenerateConfigUtils {
-
-}
--- src/main/java/rml/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package rml.dao;
-
-import rml.model.User;
-
-public interface UserMapper {
-    int insert(User record);
-
-    int insertSelective(User record);
-}
