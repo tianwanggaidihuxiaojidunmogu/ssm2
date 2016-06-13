15841051d13145a0d08c424bb39cb26475361137
laupaul <l_m_peng@163.com>
2016-06-13 17:06:53 +0800

    commit

--- src/test/java/test/racing/trans/MtbatisTest.java
+++ src/test/java/test/racing/trans/MtbatisTest.java
@@ -1,36 +1,42 @@
-package test.racing.schedule;
+package test.racing.trans;
 
-import java.util.UUID;
-
-import org.apache.ibatis.type.JdbcType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.racing.model.system.Menu;
 import com.racing.service.schedule.ScheduleService;
+import com.racing.service.system.MenuService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:context/spring-config.xml","classpath:context/spring-mybatis.xml"})
-public class ScheduleTest {
+public class MtbatisTest {
 
+	@Autowired
+	MenuService menuService;
+	
 	@Autowired
 	ScheduleService scheduleService;
 	
 	@Test
-	public void addSchedule(){
+	public void insert(){
 //		Schedule schedule=new Schedule();
-//		schedule.setJobId(UUID.randomUUID().toString());
-//		schedule.setJobName("测试任务");
-//		schedule.setJobGroup("测试");
-//		schedule.setJobClass("com.racing.schedule.TestJob.class");
-//		schedule.setJobMethod("test");
-//		schedule.setJobCron("0/1 * * * * * ?");
-//		schedule.setJobStatus(0);
-//		schedule.setJobDescription("测试");
+//		schedule.setJobId("asdaf");
+//		schedule.setJobClass("asdasd");
+//		schedule.setJobCron("aaa");
+//		schedule.setJobGroup("");
 //		scheduleService.insert(schedule);
+
+		Menu menu = new Menu();
+		menu.setId(1);
+		menu.setLevl(1);
+		menu.setName("系统菜单");
+		menu.setParent(0);
+		menu.setSeq(1);
+		menu.setUrl("#");
+		menuService.insert(menu);
 		
-//		scheduleService.deleteByPrimaryKey("5387491f-1198-4749-a3e6-ee10a82166ae");
 	}
 }
